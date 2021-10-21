75fe9645b1a9791446d68c63bccc82b840b8a5c8
mziani <mohammed.ziani.pro@gmail.com>
2021-10-21 16:31:05 +0200

    statusbar

--- costumeHalloween/src/garde/Garde.tsx
+++ costumeHalloween/src/garde/Garde.tsx
@@ -1,12 +1,14 @@
 import {View , Text, Image, Button} from "react-native";
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './style/garde.style';
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/core";
+import { StatusBar } from "react-native";
 
 
 export function Garde() {
     const navigation = useNavigation();
+    useEffect (()=>{StatusBar.setHidden(true);},[])
     return (<View style={styles.background}>
         <Image style={styles.image} source={require('../assets/logoGarde.png')} />
         <View style={styles.buttonBg}>
@@ -15,5 +17,8 @@ export function Garde() {
         </TouchableOpacity>
         </View>
         <Image style={styles.deco} source={require('../assets/decoGarde.png')} />
-    </View>);
+
+            </View>)
+    
+    
 }
